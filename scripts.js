bae8789e7983fe28d1a3e1328a966ffa3d14a673
generalpask <32198850+generalpask@users.noreply.github.com>
2019-05-15 16:10:52 +0200

    stuff added

--- scripts.js
+++ scripts.js
@@ -18,13 +18,34 @@ document.addEventListener('keydown', function(event) {
 
     const key = document.getElementById(event.code);
 
+    var unusedKeys = ["", "Escape", "F1", "F2", "F3", "F4", "F5", "F6", "F7", "F8", "F9", "F10", "F11", "F12", "Insert", "Delete", "Tab", "CapsLock", "ShiftLeft", "ShiftRight", "ControlLeft", "MetaLeft", "AltLeft", "AltRight", "ControlRight", "PageUp", "ArrowUp", "PageDown", "ArrowLeft", "ArrowDown", "ArrowRight"]
+
+    var specialKeys = []
+
     if (event.code == 'Enter') {
         $("#EnterTop, #EnterBottom").addClass('pressed');
     }
     else {
         $(key).addClass('pressed');
     }
-
+    
+    if (event.code == 'Backspace') {
+        $('.input').text(function (_,txt) {
+            return txt.slice(0, -1);
+        });
+    }
+    else if (event.code == "Enter") {
+        $('.input').append("<br\>");
+    }
+    else if (unusedKeys.includes(event.code)) {
+        return false;
+    }
+    else {
+        $(".input").append(event.key);
+    }
+    
+    
+    
 }, true);
 
 document.addEventListener('keyup', function(event) {
@@ -37,4 +58,4 @@ document.addEventListener('keyup', function(event) {
     else {
         $(key).removeClass('pressed');
     }
-})
+}, true);
