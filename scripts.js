74b6c68f5eb21b0df364c62e31cff26ff34a6101
generalpask <32198850+generalpask@users.noreply.github.com>
2019-05-16 14:04:17 +0200

    more stuff added/changed

--- scripts.js
+++ scripts.js
@@ -12,13 +12,14 @@ $("button.key").on('mouseup', function(){
     }
 });
 
+// Listeners keydown och keyup känner av när användaren trycker ner en tangent på tangentborder på sin dator
 document.addEventListener('keydown', function(event) {
     
     console.log("key = " + event.key + " and code = " + event.code);
 
     const key = document.getElementById(event.code);
 
-    var unusedKeys = ["", "Escape", "F1", "F2", "F3", "F4", "F5", "F6", "F7", "F8", "F9", "F10", "F11", "F12", "Insert", "Delete", "Tab", "CapsLock", "ShiftLeft", "ShiftRight", "ControlLeft", "MetaLeft", "AltLeft", "AltRight", "ControlRight", "PageUp", "ArrowUp", "PageDown", "ArrowLeft", "ArrowDown", "ArrowRight"]
+    var unusedKeys = ["", "Escape", "F1", "F2", "F3", "F4", "F5", "F6", "F7", "F8", "F9", "F10", "F11", "F12", "Insert", "Delete", "Equal", "BracketRight", "Tab", "Enter", "CapsLock", "ShiftLeft", "ShiftRight", "ControlLeft", "MetaLeft", "AltLeft", "AltRight", "ControlRight", "PageUp", "ArrowUp", "PageDown", "ArrowLeft", "ArrowDown", "ArrowRight"]
 
     var specialKeys = []
 
@@ -34,17 +35,18 @@ document.addEventListener('keydown', function(event) {
             return txt.slice(0, -1);
         });
     }
+    /*
     else if (event.code == "Enter") {
         $('.input').append("<br\>");
     }
-    else if (unusedKeys.includes(event.code)) {
-        return false;
+    */
+
+    else if (unusedKeys.includes(event.code)) { // Om tangenten inte ska användas,
+        return false;                           // printa inget
     }
-    else {
-        $(".input").append(event.key);
+    else {                              // Annars, 
+        $(".input").append(event.key);  // printa till input
     }
-    
-    
     
 }, true);
 
@@ -59,3 +61,9 @@ document.addEventListener('keyup', function(event) {
         $(key).removeClass('pressed');
     }
 }, true);
+
+// Får linjen att blinka
+var intervalID = window.setInterval(myCallback, 500);
+function myCallback() {
+    $('.line').toggleClass('hidden');
+}
